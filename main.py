c4cb61ee87637bfc0e05195ff16b1fc2d5abd6fc
dan2017558 <hexogaming123@gmail.com>
2025-08-02 23:29:52 -0500

    feat(window): correctly blit all items on window

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import pygame
 import config
 import item
 from tools import Hand, Knife, Sauce
+from build_sushi import Mat
 from ingredient import Tray, Ingredient
+from orders import Order
 
 # assets
 belt = pygame.image.load(os.path.join("assets", "belt.png"))
@@ -17,17 +19,19 @@ def game():
     clock = pygame.time.Clock()
     running = True
     while running:
+        config.internal_surface.fill((0, 0, 0, 0))  # reset
+        config.text_surface.fill((0, 0, 0, 0))  # reset
         hand.update()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
             elif event.type == pygame.VIDEORESIZE:  # window resize
                 config.rescale_screen(event.w, event.h)
 
-        # Drawing
-        config.internal_surface.fill((0, 0, 0))  # reset
 
+        # Drawing
         # conveyor belt
         global distance, belt_speed, belt_circumference
         visual_displacement = distance % config.INTERNAL_WIDTH
@@ -41,17 +45,28 @@ def game():
 
         # items
         for object in item.items:
-            if object != hand.item:  # not what the player is holding
+            if type(object) is Order:  # update order trays
+                object.update(belt_circumference, belt_speed)
+
+            elif type(object) is Mat:
+                config.internal_surface.blit(object.image, object.rect.topleft)
+                config.internal_surface.blit(object.food_image, object.rect.topleft + pygame.Vector2(2, 1))
+
+            elif object != hand.item:  # not what the player is holding
                 config.internal_surface.blit(object.image, object.rect.topleft)
 
         # hand + item
         if hand.item:
             config.internal_surface.blit(hand.item.image, hand.item.rect.topleft)
+
+            if type(hand.item) is Mat:  # update the position of the food within the mat
+                config.internal_surface.blit(hand.item.food_image, hand.item.rect.topleft + pygame.Vector2(2, 1))
         config.internal_surface.blit(hand.image, hand.rect.topleft)
         # Update Window
         config.win.fill((0, 0, 0))
         config.scaled_surface = pygame.transform.scale_by(config.internal_surface, config.scale)
         config.win.blit(config.scaled_surface, config.letter_box_offset)
+        config.win.blit(config.text_surface, config.letter_box_offset)
 
         pygame.display.flip()
         clock.tick(30)  # 30 FPS
@@ -65,6 +80,7 @@ if __name__ == "__main__":
     hand = Hand()
     knife = Knife()
     sauce = Sauce()
+    mat1 = Mat()
 
     salmon_tray = Tray("salmon_tray", (4, 37))
     tuna_tray = Tray("tuna_tray", (31, 37))
@@ -77,6 +93,8 @@ if __name__ == "__main__":
     belt_speed = 1
     belt_circumference = 480
     distance = 0
+
+    order1 = Order(["tuna", "tuna"], 10)
 
     # Run main function
     game()
