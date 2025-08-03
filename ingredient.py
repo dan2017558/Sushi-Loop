8b2329d95414ea5a8a10f1de2ad6197cff7f18ac
dan2017558 <hexogaming123@gmail.com>
2025-08-03 02:07:16 -0500

    feat(ingredients): limit number of unused ingredients to 5

--- ingredient.py
+++ ingredient.py
@@ -1,15 +1,21 @@
-from item import Item
+import item
 
 
-class Ingredient(Item):
+class Ingredient(item.Item):
     def __init__(self, name: str, position: tuple[int, int]):
         super().__init__(name, position)
 
 
-class Tray(Item):
+class Tray(item.Item):
     def __init__(self, name: str, position: tuple[int, int]):
         super().__init__(name, position)
 
     def produce(self):
+        # enforce max number of ingredients allowed pulled out
+        ingredients = [x for x in reversed(item.items) if isinstance(x, Ingredient)]
+        for ingredient in ingredients[4:]:
+            item.items.remove(ingredient)
+
+        # create new ingredient
         name = self.name.removesuffix("_tray")
         return Ingredient(name, (0, 0))
